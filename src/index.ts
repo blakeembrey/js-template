7aafd03ffb5170575b7e1db18503966ef32b9041
Blake Embrey <hello@blakeembrey.com>
2019-02-16 18:51:57 -0800

    Expose `compile` function to emit strings

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,24 @@ const DATA_VAR_NAME = "data";
 export type Template<T extends object> = (data: T) => string;
 
 /**
- * Fast and simple string templates.
+ * Stringify a template into a function.
  */
-export function template<T extends object = object>(value: string) {
+export function compile(value: string, displayName = "template") {
   const str = value.replace(/"|{{[^{]+}}/g, prop => {
     if (prop === '"') return '\\"';
     return `" + ${DATA_VAR_NAME}.${prop.slice(2, -2).trim()} + "`;
   });
 
-  return new Function(DATA_VAR_NAME, `return "${str}";`) as Template<T>;
+  return `function ${displayName}(${DATA_VAR_NAME}) { return "${str}"; }`;
+}
+
+/**
+ * Fast and simple string templates.
+ */
+export function template<T extends object = object>(
+  value: string,
+  displayName?: string
+) {
+  const body = compile(value, displayName);
+  return new Function(`return (${body});`)() as Template<T>;
 }
